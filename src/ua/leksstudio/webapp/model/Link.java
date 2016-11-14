3bd9f53eaae854cea913261b9cadd6bda7fa580e
alxdashko <alxdashko@gmail.com>
2016-11-14 20:19:48 +0200

    add new classes

---
+++ src/ua/leksstudio/webapp/model/Link.java
@@ -0,0 +1,9 @@
+package ua.leksstudio.webapp.model;
+
+/**
+ * Created by alxdashko on 14.11.2016.
+ */
+public class Link {
+    private String name;
+    private String url;
+}
---
+++ src/ua/leksstudio/webapp/model/Resume.java
@@ -0,0 +1,11 @@
+package ua.leksstudio.webapp.model;
+
+import java.util.List;
+
+/**
+ * Created by alxdashko on 14.11.2016.
+ */
+public class Resume {
+    private String fullName;
+    private String location;
+}
