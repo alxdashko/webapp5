6a4d705ec574fd9d364aaf7e02640574acce3b3e
alxdashko <alxdashko@gmail.com>
2016-11-14 16:06:42 +0200

    Initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+/**
+ * Created by alxdashko on 14.11.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello !");
+    }
+}
