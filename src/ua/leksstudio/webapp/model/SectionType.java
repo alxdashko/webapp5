a2deefa9ef7c480a90532ff7defbdd3eb4add3fc
alxdashko <alxdashko@gmail.com>
2016-11-15 16:41:04 +0200

    added sectionType

--- src/ua/leksstudio/webapp/model/SectionType.java
+++ src/ua/leksstudio/webapp/model/SectionType.java
@@ -4,9 +4,19 @@ package ua.leksstudio.webapp.model;
  * Created by alxdashko on 15.11.2016.
  */
 public enum SectionType {
-    OBJECTIVE,
-    ACHIEVEMENT,
-    QUALIFICATIONS,
-    EXPERIENCE,
-    EDUCATION
+    OBJECTIVE("Позиция"),
+    ACHIEVEMENT("Достижения"),
+    QUALIFICATIONS("Квалификация"),
+    EXPERIENCE("Опыт работы"),
+    EDUCATION("Образование");
+
+    private String title;
+
+    SectionType(String title){
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
